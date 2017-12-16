574eba0e35f1a8efeb3d3da17d00ba174886a43f
Jason Fernandes <justcontactjason@gmail.com>
2017-12-15 19:09:39 -0500

    AreYouSleepy.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreYouSleepy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep have you had?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.Write("Hello " + name + "! ");
+
+            if (hoursOfSleep >= 8)
+            {
+                Console.WriteLine("You are well rested!");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
